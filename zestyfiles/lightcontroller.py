6464c0a6f513b5028915c0d08ae4983b07ea7bab
gillian <gdlaming@wustl.edu>
2018-11-08 11:50:56 -0600

    all important files

--- zestyfiles/lightcontroller.py
+++
@@ -1,9 +0,0 @@
-
-#source: https://www.instructables.com/id/Raspberry-Pi-Home-Automation-Control-lights-comput/
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setmode(GPIO.BCM
-GPIO.setup(2, GPIO.OUT)
-GPIO.output(2, False)
