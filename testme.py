e509f2f7012be462c3f17918c9af019ebd570485
GAK205 <43627247+GAK205@users.noreply.github.com>
2018-12-10 11:43:11 -0600

    Delete testme.py

--- testme.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#os.system('cd /home/pi/rpi-rgb-led-matrix/bindings/python/samples')
-#os.system("ls -ltra")
-os.system('sudo python3 /home/pi/rpi-rgb-led-matrix/bindings/python/samples/rotating-block-generator.py --led-slowdown-gpio=2 --led-rows=32 --led-cols=64 --led-brightness=30')
-
-print("testing")
